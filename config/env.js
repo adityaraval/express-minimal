f794f7091dada8a5012a0ab283334787ae38dd76
Aditya Raval <adi7017@gmail.com>
2017-07-17 17:41:31 +0530

    Added DB Field to Config Env

--- config/env.js
+++ config/env.js
@@ -1,17 +1,20 @@
 module.exports.dev = {
     "BASE_URL":"http://localhost:3000",
     "VERSION":"/api/v1",
-    "PORT":"3000"
+    "PORT":3000,
+    "DB":{}
 }
 
 module.exports.test = {
     "BASE_URL":"http://localhost:4000",
     "VERSION":"/api/v1",
-    "PORT":"4000"
+    "PORT":4000,
+    "DB":{}
 }
 
 module.exports.prod = {
     "BASE_URL":"http://localhost:5000",
     "VERSION":"/api/v1",
-    "PORT":"5000"
+    "PORT":5000,
+    "DB":{}
 }
